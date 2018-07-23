c429bb4bbaf556c83948fc6c8359fac5f1295cbe
hobbyquaker <bastiraff@googlemail.com>
2018-07-23 22:04:53 +0200

    Display node: fix convertIcon (closes #11)

--- nodes/ccu-display.js
+++ nodes/ccu-display.js
@@ -72,7 +72,7 @@ module.exports = function (RED) {
                 if (map[ico]) {
                     col = map[ico];
                 }
-                if ([].indexOf(ico) === -1) {
+                if (['0x80', '0x81', '0x82', '0x83', '0x84', '0x85', '0x86', '0x87', '0x88'].indexOf(ico) === -1) {
                     ico = '';
                 }
                 return ico;
